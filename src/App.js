d461394f38fe9acc4271ea6f1d4582f4e6f6bebd
asterjody <116712028+asterjody@users.noreply.github.com>
2023-01-19 05:03:51 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
     <div className="App">
       <h1>ALPHABET MEMORY</h1>
       <Cards />
-      <div className=''>
-      <button className="buttons" onClick={()=> window.location.reload(false)}><b>RESTART GAME</b></button>
-      </div>
+      <button className="buttons" onClick={()=> window.location.reload(false)}><b>Restart Game</b></button>
     </div>
   );
 }
